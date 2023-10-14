fbb445780dfeb57d44d381537799194b176d4002
uyak0 <milkycoffeethings@gmail.com>
2023-10-14 16:10:58 +0800

    prog

--- weather_app/src/App.vue
+++ weather_app/src/App.vue
@@ -1,10 +1,39 @@
-<script setup lang="ts">
+<script>
   import WeatherItems from './components/WeatherItems.vue';
+  import axios from 'axios';
+
+  export default {
+    name: 'app',
+    components: {
+      WeatherItems
+    },
+
+    data() {
+      return {
+        name: '',
+        temp: 0
+      }
+    },
+
+    methods: {
+      created() {
+        axios.get('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=199b2efcae35ce7c093a0c456250f838')
+        .then(
+          res => {
+              this.name = res.data.name;
+              this.temp = res.data.main.temp;
+              console.log(res)
+            }
+        )
+        .catch(err => console.log(err));
+      }
+    }
+  }
 </script>
 
 <template>
   <h1> Hello </h1>
-  <WeatherItems />
+  <WeatherItems :name="name" :temp="temp" />
 </template>
 
 <style scoped>
--- weather_app/src/components/WeatherItems.vue
+++ weather_app/src/components/WeatherItems.vue
@@ -1,28 +1,13 @@
-<script setup>
-    import { ref } from 'vue'
-    import axios from 'axios'
+<script lang="ts">
+    export default {
+        props: ['name', 'temp']
+    }
 
-    const items = ref(''),
-          temp = ref(0);
-
-    axios({
-        method: 'get',
-        url: 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=199b2efcae35ce7c093a0c456250f838',
-        responseType: 'json'
-    })
-    .then(
-        res => {
-            items.value = res.data.name;
-            temp.value = res.data.main.temp;
-            console.log(res)
-        }
-    )
-    .catch(err => console.log(err));
 </script>
 
 <template>
     <div class="container">
-        <h1>{{ items }}</h1>
+        <h1>{{ name }}</h1>
         <h2>{{ temp }}</h2>
     </div>  
 </template>
