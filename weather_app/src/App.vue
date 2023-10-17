fd776cb1d8a137d9ce4bdf346c445b69f19b1c06
uyak0 <milkycoffeethings@gmail.com>
2023-10-17 21:36:30 +0800

    +styles that doesnt quite work

--- weather_app/src/App.vue
+++ weather_app/src/App.vue
@@ -20,7 +20,7 @@
       * Retrieves the latitude and longitude coordinates for a given location.
       *
       * @param {string} location - The location for which to retrieve coordinates.
-      * @return {Promise<string>} The coordinates in the format 'lat={latitude}&lon={longitude}'.
+      * @return {Promise<string>} - The coordinates in the format 'lat={latitude}&lon={longitude}'.
       */
      async getCoord(location: string): Promise<string> {
         await axios.get('http://api.openweathermap.org/geo/1.0/direct?q=' + location + '&limit=5&appid=199b2efcae35ce7c093a0c456250f838')
@@ -39,7 +39,7 @@
      */
     async created(): Promise<void> {
       this.coord = await this.getCoord('Kuala Lumpur')
-      console.log(this.coord)
+      // console.log(this.coord)
       // console.log('App mounted');
       axios.get('https://api.openweathermap.org/data/2.5/weather?' + this.coord + '&units=metric&appid=199b2efcae35ce7c093a0c456250f838')
       .then(res => {
@@ -51,22 +51,54 @@
 </script>
 
 <template>
-  <WeatherItems :weatherData="weatherData" />
+  <WeatherItems class="weather-item" id="item1" :weatherData="weatherData" />
+  <WeatherItems class="weather-item" id="item2" :weatherData="weatherData" />
+  <WeatherItems class="weather-item" id="item3" :weatherData="weatherData" />
 </template>
 
 <style scoped>
-  /* body {
-    animation: fadeInAnimation ease 5s;
-    animation-iteration-count: 1;
-    animation-fill-mode: forwards;
+  #item1 {
+    transform: rotate(-5deg) translate(15px, 15px);
   }
- 
-  @keyframes fadeInAnimation {
-    0% {
-        opacity: 0;
-    }
-    100% {
-        opacity: 1;
-     }
-  } */
+  #item1:hover {
+    transform: scale(1.25) rotate(-10deg);
+    z-index: 0;
+  }
+  #item1:hover ~ #item3 {
+    transform: scale(0.8) translate(-30px, 40px) rotate(15deg);
+    z-index: 0;
+    filter: blur(2px);
+  }
+  #item1:hover + #item2 {
+    transform: scale(0.8) translate(50px, -10px) rotate(5deg);
+    z-index: 1;
+    filter: blur(2px);
+  }
+
+  #item2 {
+    transform: translate(0, -10px);
+    z-index: 1;
+  }
+  #item2:hover {
+    transform: scale(1.25);
+  }
+  #item2::before:hover {
+    transform: scale(0.8) translate(50px, -10px) rotate(5deg);
+    filter: blur(2px);
+  }
+  #item2:hover + #item3 {
+    transform: scale(0.8) translate(-30px, 40px) rotate(15deg);
+    filter: blur(2px);
+  }
+  #item2:hover  {
+    
+  }
+
+  #item3 {
+    transform: translate(-15px, 15px) rotate(5deg);
+  }
+  #item3:hover {
+    transform: scale(1.1) rotate(1deg);
+    z-index: 1;
+  }
 </style>
--- weather_app/src/components/WeatherItems.vue
+++ weather_app/src/components/WeatherItems.vue
@@ -8,17 +8,24 @@
 <template>
     <div class="weather-item">
         <h1>{{ weatherData.name }}</h1>
+        <h2>{{ weatherData.sys.country  }}</h2>
         <h3>{{ weatherData.main.temp }}</h3>
     </div>  
 </template>
 
 <style scoped>
     .weather-item {
+        background-color: white;
+        text-align: center;
         border: 1px solid grey;
-        padding: 10px;
+        padding: 10rem 20px 12.5px;
         border-radius: 10px;
+        transition: 0.5s;
     }
     .weather-item:hover {
         background-color: lightgrey;
+        color: darkslategrey;
+        transition: 0.5s;
+        cursor: default;
     }
 </style>
