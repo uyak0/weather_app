46647c173d7a05c8d4b00522e5acf0186fb2dfc6
uyak0 <milkycoffeethings@gmail.com>
2023-10-21 15:24:44 +0800

    +prog

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,23 +2,32 @@
     <div>
         <nav>
             <img class="logo" src="../assets/weather-icon.png">
+            <input type="text" placeholder="Add location...">
         </nav>
     </div>
 </template>
 
 <style scoped>
+    input {
+        display: block;
+        margin: auto;
+        margin-bottom: 10px;
+        border-radius: 10px;
+        border-style: solid;   
+    }
     img {
         display: block;
         width: 65px;
-        margin: auto;
+        margin: 0px 0px 20px 0px;
     }
     nav {
-        position:fixed;
+        display: grid;
+        align-items: left;
+        position: fixed;
         top: 0;
         left: 0;
         min-width: 100%;
         min-height: 2.5rem;
-        top: 0;
         margin: 0;
         padding: 0;
         background-color: white;
