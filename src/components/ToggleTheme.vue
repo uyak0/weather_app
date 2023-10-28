877fe271f665329ac58b8fb808e84bbc40c91eba
uyak0 <milkycoffeethings@gmail.com>
2023-10-28 11:14:43 +0800

    +some css; tried to separate searchbar component from header

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -19,6 +19,7 @@
         <input type="checkbox" value="" class="sr-only peer">
         <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-0 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
         
+        <!-- Moon icon -->
         <div class="absolute left-12 w-3/6 ml-1.5">
             <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" :class="{ 'fill-gray-900': isToggled, 'fill-gray-400': !isToggled }">
                 <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
@@ -31,5 +32,6 @@
                 </g>
             </svg>
         </div>
+
     </label>
 </template>
--- src/components/WeatherItems.vue
+++ src/components/WeatherItems.vue
@@ -15,6 +15,7 @@
 
 <template>
     <div class="weather-item relative rounded-lg w-64 h-64 pt-20 flex-col justify-center items-center bg-white text-center cursor-pointer shadow-md hover:shadow-2xl hover:-translate-y-4" :class="{ hidden : !showDiv, flex: showDiv}" @click="showDiv = false">
+        <!-- Close button -->
         <div class="absolute top-2 left-2">    
             <svg xmlns="http://www.w3.org/2000/svg" width="15px" viewBox="0 0 24 24" fill="#9ca3af">
                 <path d="M5 5L19 19M5 19L19 5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
