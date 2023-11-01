7ae1175a0419059335e1490a8cb90493205c7444
uyak0 <milkycoffeethings@gmail.com>
2023-11-01 20:10:09 +0800

    +weather icons by complete accident

--- src/components/WeatherItems.vue
+++ src/components/WeatherItems.vue
@@ -14,7 +14,7 @@
 </script>
 
 <template>
-    <div class="transition ease-in-out duration-500 hover:bg-gray-100 border-solid border dark:border-0 relative rounded-lg w-64 h-64 pt-20 flex-col justify-center items-center bg-white dark:bg-base dark:text-text text-center cursor-pointer shadow-md hover:shadow-2xl hover:-translate-y-4" :class="{ hidden : !showDiv, flex: showDiv}" @click="showDiv = false">
+    <div class="transition ease-in-out duration-500 hover:bg-gray-100 border-solid border dark:border-0 relative rounded-lg w-64 h-64 flex-col justify-center items-center bg-white dark:bg-base dark:text-text text-center cursor-pointer shadow-md hover:shadow-2xl hover:-translate-y-4" :class="{ hidden : !showDiv, flex: showDiv}" @click="showDiv = false">
         <!-- Close button -->
         <div class="fill-[#9ca3af] dark:fill-text absolute top-2 left-2">    
             <svg class="fill-text" xmlns="http://www.w3.org/2000/svg" width="15px" viewBox="0 0 24 24">
@@ -22,8 +22,14 @@
             </svg>
         </div>
 
-        <p class="font-semibold text-2xl">{{ weatherData.name }}</p>
-        <p class="font-light text-xl">{{ weatherData.sys.country }}</p>
-        <p class="font-thin">{{ weatherData.main.temp }} °C</p>
+        <!-- Weather icon -->
+        <img class="w-32" :src="`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`" alt="weather icon">
+
+        <!-- Weather data -->
+        <div class="flex flex-col">
+            <p class="font-semibold text-2xl">{{ weatherData.name }}</p>
+            <p class="font-light text-xl">{{ weatherData.sys.country }}</p>
+            <p class="font-thin">{{ weatherData.main.temp }} °C</p>
+        </div>
     </div>  
 </template>
