636f5c4056abb25e2744ffd311695d9c9e702e21
uyak0 <milkycoffeethings@gmail.com>
2023-11-02 15:07:26 +0800

    +delete button for weather cards

--- src/App.vue
+++ src/App.vue
@@ -103,7 +103,7 @@
 
       <div class="flex flex-wrap grid-cols-5 gap-2 my-5 mx-5">
         <template v-for="(data, index) in weatherData">
-          <WeatherItems :id="'item' + index" :weatherData="data"/>
+          <WeatherItems :id="'item' + index" :weatherData="data" @delete-item="weatherData.splice(index,1), coords.splice(index, 1)"/>
         </template>
       </div>  
     </div>
--- src/components/WeatherItems.vue
+++ src/components/WeatherItems.vue
@@ -2,6 +2,7 @@
     export default {
         name: "WeatherItems",
         props: ["weatherData"],
+        emits:["delete-item"],
         data() {
             return {
                 showDiv: true 
@@ -14,14 +15,14 @@
 </script>
 
 <template>
-    <div class="transition ease-in-out duration-500 hover:bg-gray-100 border-solid border dark:border-0 relative rounded-lg w-64 h-64 flex-col justify-center items-center bg-white dark:bg-base dark:text-text text-center cursor-pointer shadow-md hover:shadow-2xl hover:-translate-y-4" :class="{ hidden : !showDiv, flex: showDiv}" @click="showDiv = false">
+    <div class="transition ease-in-out duration-500 hover:bg-gray-100 border-solid border dark:border-base relative rounded-lg w-64 h-64 flex-col justify-center items-center bg-white dark:bg-base dark:text-text text-center cursor-pointer shadow-md hover:shadow-2xl hover:-translate-y-4 flex"  >
         <!-- Close button -->
-        <div class="fill-[#9ca3af] dark:fill-text absolute top-2 left-2">    
-            <svg class="fill-text" xmlns="http://www.w3.org/2000/svg" width="15px" viewBox="0 0 24 24">
+        <div class="absolute top-2 left-2 hover:bg-slate-400 dark:hover:bg-overlay0 transition ease-in-out duration-500 rounded-full py-1 px-1" @click="$emit('delete-item')">    
+            <svg xmlns="http://www.w3.org/2000/svg" width="15px" viewBox="0 0 24 24">
                 <path d="M5 5L19 19M5 19L19 5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
         </div>
-
+        
         <!-- Weather icon -->
         <img class="w-32" :src="`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`" alt="weather icon">
 
